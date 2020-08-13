d5d3bd2b8c7b5067f0cff126d1762b144999c9e8
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-08-13 17:54:49 -0300

    ForecastCard creation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import SearchBar from "./components/SearchBar";
 import WeatherCard from "./components/WeatherCard";
+import ForecastCard from "./components/ForecastCard";
 
 function App() {
   return (
@@ -14,7 +15,26 @@ function App() {
         <h2 className="mr-5">Forecast</h2>
         <h2>Actual Time</h2>
       </div>
-      <WeatherCard />
+      {/* <WeatherCard /> */}
+      <div className="container forecast ">
+        <div className="row justify-content-center ">
+          <div className="col-lg-2">
+            <ForecastCard />
+          </div>
+          <div className="col-lg-2">
+            <ForecastCard />
+          </div>
+          <div className="col-lg-2">
+            <ForecastCard />
+          </div>
+          <div className="col-lg-2">
+            <ForecastCard />
+          </div>
+          <div className="col-lg-2">
+            <ForecastCard />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
